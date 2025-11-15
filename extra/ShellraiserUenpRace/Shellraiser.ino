12ab8b6e5f4737342feefdde9e665a1735cf1365
Eduardo Medeiros da Paz <eduardomedeirosdapaz@gmail.com>
2025-11-14 22:31:01 -0300

    Manutenção nos comentários do código do carrinho.

--- extra/ShellraiserUenpRace/Shellraiser.ino
+++ extra/ShellraiserUenpRace/Shellraiser.ino
@@ -1,17 +1,17 @@
-//Definição dos pinos de controle do motor
+//Definindo os pinos de controle do motor
 #define M1 9 // Pino_Velocidade 1º Motor (0 a 255) - Porta IN2 ponte H
 #define M2 11 // Pino_Velocidade 2º Motor (0 a 255) - Porta IN4 ponte H
 #define direcao1 8 // Pino_Direção do 1º Motor: Para frente / Para trás (HIGH ou LOW) - porta IN1 ponte H
 #define direcao2 10 // Pino_Direção do 2º Motor: Para frente / Para trás (HIGH ou LOW) - porta IN3 ponte H
 
-//Definição dos pinos dos sensores, baseando-se na placa do arduíno.
+//Definindo os pinos dos sensores, baseando-se na placa do arduíno.
 #define pin_S1 41
 #define pin_S2 43
 bool Sensor1 = 0;
 bool Sensor2 = 0;
 
-//Variável responsável por controlar a velocidade dos motores, podendo ser alterada de acordo com sua vontade.
-int velocidade = 125;
+//Variável que a velocidade dos motores, podendo ser alterada de acordo com sua vontade.
+int velocidade = 150;
 
 void setup() {
   //Colocamos os pinos de controle dos motores como saída
@@ -47,7 +47,7 @@ void loop() {
     analogWrite(M1, velocidade);
     analogWrite(M2, 0);
   }
-  else if ((Sensor1 == 0) && (Sensor1 == 0)) { // Ambos sensores detectam preto (opcional: parar, depende da escolha de lógica preferida).
+  else if ((Sensor1 == 0) && (Sensor1 == 0)) { // Os dois motoresdetectam preto (opcional: parar, depende da escolha de lógica preferida), neste caso, passam direto.
     analogWrite(M1, velocidade);
     analogWrite(M2, velocidade);
   }
